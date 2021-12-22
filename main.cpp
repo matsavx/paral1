2bc4db17ce51f85a17ee562d5193296b7927997e
Matsavx <super.matvey0111@gmail.com>
2021-12-23 01:56:03 +1000

    Added last functions

--- main.cpp
+++ main.cpp
@@ -141,18 +141,190 @@ double integrate(double a, double b, f_t f) {
     return Result * dx;
 }
 
+static unsigned num_treads = std::thread::hardware_concurrency();
+
+unsigned get_num_threads()
+{
+	return num_treads;
+}
+
+#ifdef __cpp_lib_hardware_interface_size
+using std::hardware_constructive_interface_size;
+    using std::hardware_destructive_interface_size;
+#else
+constexpr std::size_t hardware_constructive_interface_size = 64u;
+constexpr std::size_t hardware_destructive_interface_size = 64u;
+#endif
+
+std::size_t ceil_div(std::size_t x, std::size_t y);
+
+template <class ElementType, class BinaryFn>
+ElementType reduce_vector(const ElementType* V, std::size_t n, BinaryFn f, ElementType zero);
+
+template <class ElementType, class UnaryFn, class BinaryFn>
+ElementType reduce_range(ElementType a, ElementType b, std::size_t n, UnaryFn get, BinaryFn reduce_2, ElementType zero)
+{
+	unsigned T = get_num_threads();
+	struct reduction_partial_result_t
+	{
+		alignas(hardware_destructive_interface_size) ElementType value;
+		
+		explicit reduction_partial_result_t(ElementType value)
+		{
+			value = value;
+		}
+	};
+	static auto reduction_partial_results =
+			std::vector<reduction_partial_result_t>(std::thread::hardware_concurrency(), reduction_partial_result_t{zero});
+	
+	constexpr std::size_t k = 2;
+	auto thread_proc = [=](unsigned t)
+	{
+		auto K = ceil_div(n, k);
+		double dx = (b - a) / n;
+		std::size_t Mt = K / T;
+		std::size_t it1;
+		
+		if(t < (K % T))
+		{
+			it1 = ++Mt * t;
+		}
+		else
+		{
+			it1 = (K % T) * Mt + t;
+		}
+		it1 *= k;
+		std::size_t mt = Mt * k;
+		std::size_t it2 = it1 + mt;
+		
+		ElementType accum = zero;
+		for(std::size_t i = it1; i < it2; i++)
+		{
+			accum = reduce_2(accum, get(a + i*dx));
+		}
+		
+		reduction_partial_results[t].value = accum;
+	};
+	
+	auto thread_proc_2_ = [=](unsigned t, std::size_t s)
+	{
+		if(((t % (s * k)) == 0) && (t + s < T))
+		{
+			reduction_partial_results[t].value =
+					reduce_2(reduction_partial_results[t].value, reduction_partial_results[t + s].value);
+		}
+	};
+	
+//	std::vector<std::thread> threads;
+//	for(unsigned t = 1; t < T; t++)
+//	{
+//		threads.emplace_back(thread_proc, t);
+//	}
+//	thread_proc(0);
+//	for(auto& thread : threads)
+//	{
+//		thread.join();
+//	}
+//
+//	std::size_t s = 1;
+//	while(s < T)
+//	{
+//		for(unsigned t = 1; t < T; t++)
+//		{
+//			threads[t-1] = std::thread(thread_proc_2_, t, s);
+//		}
+//		thread_proc_2_(0, s);
+//		s *= k;
+//
+//		for(auto& thread : threads)
+//		{
+//			thread.join();
+//		}
+//	}
+	
+//	return reduction_partial_results[0].value;
+}
+
+double integrate_crit(double a, double b, function f)
+{
+	double result = 0, dx = (b - a)/STEPS;
+
+#pragma omp parallel
+	{
+		double R = 0;
+		unsigned t = (unsigned) omp_get_thread_num();
+		unsigned T = (unsigned) omp_get_num_threads();
+		
+		for (unsigned i = t; i < STEPS; i += T)
+		{
+			R += f(i * dx + a);
+		}
+#pragma omp critical
+		result += R;
+	}
+	
+	return result * dx;
+}
+
+double integrate_range_reduction(double a, double b, function f) {
+	double dx = (b - a)/STEPS;
+	return reduce_range(a, b, STEPS, f, [](auto x, auto y){return x + y;}, 0.0)*dx;
+}
+
+double integrate_false_sharing(double a, double b, function f)
+{
+	unsigned T;
+	double result = 0, dx = (b - a) / STEPS;
+	double* accum;
+
+#pragma omp parallel shared(accum, T)
+	{
+		unsigned t = (unsigned) omp_get_thread_num();
+#pragma omp single
+		{
+			T = (unsigned) omp_get_num_threads();
+			accum = (double*) calloc(T, sizeof(double));
+		}
+		
+		for (unsigned i = t; i < STEPS; i += T)
+		{
+			accum[t] += f(dx*i + a);
+		}
+	}
+	
+	for (unsigned i = 0; i < T; ++i) {
+		result += accum[i];
+	}
+	
+	return result * dx;
+}
+
 int main() {
 //    cout << integrate(-1,1,func) << endl;
 //    std::cout << integratePS(-1,1,func)<<std::endl;
 	std::cout << "integrate\n";
 	show_experiment_results(integrate);
+	std::cout << endl;
 	std::cout << "integrate_cpp_mtx\n";
     show_experiment_results(integrate_cpp_mtx);
+	std::cout << endl;
 	std::cout << "integrate_partial_sum\n";
 	show_experiment_results(integratePS);
+	std::cout << endl;
 	std::cout << "integrate_partial_sum_omp\n";
 	show_experiment_results(integrate_partial_sum);
+	std::cout << endl;
 	std::cout << "integrate_reduction\n";
 	show_experiment_results(integrate_reduction);
+	std::cout << endl;
+	std::cout << "integrate_range_reduction\n";
+	show_experiment_results(integrate_range_reduction);
+	std::cout << endl;
+	std::cout << "integrate_crit_omp\n";
+	show_experiment_results(integrate_crit);
+	std::cout << endl;
+	std::cout << "integrate_false_sharing\n";
+	show_experiment_results(integrate_false_sharing);
+	std::cout << endl;
 //    experiment_result_t ExpP = run_experiment(integratePS);
 }
